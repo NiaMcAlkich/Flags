240c73a9f25e16623aa31a144c7173acdd597b52
Nia McAlkich <niamcalkich@gmail.com>
2023-04-23 19:22:09 -0400

    I think I fixed all bugs now committing so I don't lose my changes

--- FlagGame/src/app/flaggame/flaggame.component.ts
+++ FlagGame/src/app/flaggame/flaggame.component.ts
@@ -1,47 +1,77 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-flaggame',
   templateUrl: './flaggame.component.html',
   styleUrls: ['./flaggame.component.css']
 })
 
-
 export class FlaggameComponent {
-
-  flagsToDisplay: number[] = [];     //This is an array for the 3 radom flags that get picked for display
-  randomCorrectAnswer: number;     //This is the variable used to pick 1 of th 3 display flags to be the correct answer
+  flagsToDisplay: number[] = [];
   countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"];
-  //This maps the images in the assets file to the array so when a flag is picked it will display the right flags 
   imageUrls = [
-    '../assets/Estonia@3x.png',
-    '../assets/France@3x.png',
-    '../assets/Germany@3x.png',
-    '../assets/Ireland@3x.png',
-    '../assets/Italy@3x.png',
-    '../assets/Nigeria@3x.png',
-    '../assets/Poland@3x.png',
-    '../assets/Russia@3x.png',
-    '../assets/Spain@3x.png',
-    '../assets/UK@3x.png',
-    '../assets/US@3x.png',
+    '../assets/Estonia@2x.png',
+    '../assets/France@2x.png',
+    '../assets/Germany@2x.png',
+    '../assets/Ireland@2x.png',
+    '../assets/Italy@2x.png',
+    '../assets/Nigeria@2x.png',
+    '../assets/Poland@2x.png',
+    '../assets/Russia@2x.png',
+    '../assets/Spain@2x.png',
+    '../assets/UK@2x.png',
+    '../assets/US@2x.png'
   ];
-  correctAnswer: string;     //Variable holding the correct flag
-  score = 0;     //Keeps score
-  counter = 0;     //Keeps count of questions
+  correctFlagIndex!: number;
+  correctAnswer: string = '';
+  score: number = 0;
+  counter: number = 0;
 
-  //This is the while loop for picking 3 random flags for the array to choose from and 
-  //it will make sure there are no repeats in the array 
   constructor() {
+    this.flagPicker();
+  }
+
+  flagPicker() {
+    this.flagsToDisplay = [];
     while (this.flagsToDisplay.length < 3) {
-      const flag = Math.floor(Math.random() * 11);
+      const flag = Math.floor(Math.random() * this.countries.length);
       if (!this.flagsToDisplay.includes(flag)) {
         this.flagsToDisplay.push(flag);
       }
     }
-    this.randomCorrectAnswer = Math.floor(Math.random() * 3);     //These pick the 3 random numbers and then 1 to be the right answer
-    this.correctAnswer = this.countries[this.flagsToDisplay[this.randomCorrectAnswer]];
+    this.correctFlagIndex = Math.floor(Math.random() * this.flagsToDisplay.length);
+    this.correctAnswer = this.countries[this.flagsToDisplay[this.correctFlagIndex]];
   }
 
+  flagTapped(flagIndex: number) {
+    this.counter++;
+  
+    const correctFlagIndex = this.flagsToDisplay[this.correctFlagIndex];
+  
+    if (flagIndex === correctFlagIndex) {
+      alert(`Correct!`);
+      this.score++;
+    } 
+    
+    else if (this.flagsToDisplay.includes(flagIndex)) {
+      alert(`Wrong, you clicked the flag for ${this.countries[flagIndex]}`);
+    }
+  
+    if (this.counter === 8) {
+      alert(`Game over your score was ${this.score}/8`);
+      this.reset();
+    } 
+    
+    else {
+      this.flagPicker();
+    }
+    
+  }
+  
+  
+  reset() {
+    this.counter = 0;
+    this.score = 0;
+    this.flagPicker();
+  }
 }
